65715bd5c6c921fdb2feef4a5ff8a828742595c1
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-05-02 13:16:18 +0900

    define schema but error 1064 has occured when mysql -uroot yashulan < sql/mysql.sql

--- lib/Yashulan/DB/Schema.pm
+++ lib/Yashulan/DB/Schema.pm
@@ -8,9 +8,27 @@ use Teng::Schema::Declare;
 base_row_class 'Yashulan::DB::Row';
 
 table {
-    name 'member';
+    name 'station';
     pk 'id';
     columns qw(id name);
+}
+
+table {
+    name 'user';
+    pk 'id';
+    columns qw(id name password);
+};
+
+
+table {
+    name 'restaurant';
+    pk 'id';
+    columns qw(id name station genre budget_lower budget_upper lunch_or_dinner star good tabelog author created_at updated_at);
+};
+table {
+    name 'favorite';
+    pk 'id';
+    qw(id user_id restaurant_id created_at);
 };
 
 1;
--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,34 @@
-CREATE TABLE IF NOT EXISTS member (
+CREATE TABLE IF NOT EXISTS user (
     id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name         VARCHAR(255)
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    password     VARCHAR(255) NOT NULL,
+    created_at   VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS station (
+    id              INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS restaurant (
+    id              INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(255) NOT NULL UNIQUE,
+    station         INTEGER NOT NULL,
+    genre           VARCHAR(255) NOT NULL,
+    budget_lower    INTEGER,
+    budget_upper    INTEGER,
+    lunch_or_dinner INTEGER,
+    star            FLOAT NOT NULL,
+    tabelog         VARCHAR(255),
+    author          INTEGER NOT NULL,
+    created_at      VARCHAR(255) NOT NULL,
+    updated_at      VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS favorite (
+    id             INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id        INTEGER NOT NULL,
+    restaurant_id  INTEGER NOT NULL,
+    created_at     VARCHAR NOT NULL
+);
+
