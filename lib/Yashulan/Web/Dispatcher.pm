84c6be5e44b18e1fa93b694d9a2bf790867003c4
imomochi/toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-05-28 23:43:04 +0900

    Update Dispatcher.pm

--- lib/Yashulan/Web/Dispatcher.pm
+++ lib/Yashulan/Web/Dispatcher.pm
@@ -4,8 +4,13 @@ use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
 
-get '/api/restaurants/:restaurant_id'    => 'Restaurant#get_the_restaurant';
-get '/api/restaurants'                   => 'Restaurant#get_restaurants';
+use Yashulan::Web::C::Restaurant;
+use Yashulan::Web::C::Account;
+
+base 'Yashulan::Web::C';
+
+get  '/api/restaurants/:restaurant_id'   => 'Restaurant#get_the_restaurant';
+get  '/api/restaurants'                  => 'Restaurant#get_restaurants';
 post '/api/restaurants/favorite'         => 'Restaurant#get_favorites_of_the_user';
 post '/api/account/signup'               => 'Account#signup';
 post '/api/account/login'                => 'Account#login';
