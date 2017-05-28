4fb511575cfc6fd9dafe9b9c7f722c0f0385cca1
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-05-29 00:01:04 +0900

    create Repository skelton

---
+++ lib/Yashulan/Repository/Restaurant.pm
@@ -0,0 +1,21 @@
+package Yashulan::Repository::Restaurant;
+use strict;
+use utf8;
+use warnings;
+use Yashulan;
+
+sub db { Yashulan->context->db };
+
+sub fetch_by_restaurant_id {
+
+}
+
+sub fetch_all_restaurants {
+
+}
+
+sub fetch_favorites_by_userid {
+
+}
+
+1;
