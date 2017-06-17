5594c6eb419986498b5836fbd8918e77546a7962
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-06-17 23:43:44 +0900

    add pager method to all restaurant list

--- app/src/Header.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="header">
-    <router-link :to="{ name: 'Root' }">一覧へ</router-link>
-    <router-link :to="{ name: 'Login' }">ログイン</router-link>
-    <router-link :to="{ name: 'Signin'}">ユーザー登録</router-link>
-  </div>
-</template>
